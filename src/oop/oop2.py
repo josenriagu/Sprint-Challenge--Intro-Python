69a3440d342738a37b742a9aab240dfe7a4196da
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-02-14 10:11:42 +0100

    feat(python): oop challenge completed - add new assertions to test suite

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,11 +3,19 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def __str__(self):
+        return str(self.num_wheels)
+
+    def drive(self):
+        return "vroooom"
+
+    def drive_faster(self):
+        return "vroom vrooooom vroooooooom!!"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -17,7 +25,17 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
-# TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels)
+        super().__str__()
+
+    def drive(self):
+        return "BRAAAP!!"
+
+    def drive_faster(self):
+        return "BRAAAAAP!! TATAAATTAATTTAA!!"
+
 
 vehicles = [
     GroundVehicle(),
@@ -29,4 +47,6 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for vehicle in vehicles:
+    print(vehicle.drive())
+    print(vehicle)  # calls the __str__ method, which will print num_wheels
--- src/oop/test_oop2.py
+++ src/oop/test_oop2.py
@@ -1,26 +1,30 @@
 import unittest
-from oop2 import *
+from oop2 import GroundVehicle, Motorcycle
+
 
 class Oop2Tests(unittest.TestCase):
-  def setUp(self):
-    self.ground_vehicle = GroundVehicle()
-    self.motorcycle = Motorcycle()
+    def setUp(self):
+        self.ground_vehicle = GroundVehicle()
+        self.motorcycle = Motorcycle()
 
-  def test_motorcycle_inheritance(self):
-    self.assertTrue(isinstance(self.motorcycle, GroundVehicle))
+    def test_motorcycle_inheritance(self):
+        self.assertTrue(isinstance(self.motorcycle, GroundVehicle))
 
-  def test_ground_vehicle_num_wheels(self):
-    self.assertEqual(self.ground_vehicle.num_wheels, 4)
+    def test_ground_vehicle_num_wheels(self):
+        self.assertEqual(self.ground_vehicle.num_wheels, 4)
 
-  def test_motocycle_num_wheels(self):
-    self.assertEqual(self.motorcycle.num_wheels, 2)
+    def test_motocycle_num_wheels(self):
+        self.assertEqual(self.motorcycle.num_wheels, 2)
 
-  def test_ground_vehicle_drive(self):
-    self.assertEqual(self.ground_vehicle.drive(), "vroooom")
+    def test_ground_vehicle_drive(self):
+        self.assertEqual(self.ground_vehicle.drive(), "vroooom")
+        self.assertEqual(self.ground_vehicle.drive_faster(), "vroom vrooooom vroooooooom!!")
+        
 
-  def test_motorcyle_drive(self):
-    self.assertEqual(self.motorcycle.drive(), "BRAAAP!!")
+    def test_motorcyle_drive(self):
+        self.assertEqual(self.motorcycle.drive(), "BRAAAP!!")
+        self.assertEqual(self.motorcycle.drive_faster(), "BRAAAAAP!! TATAAATTAATTTAA!!")
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
